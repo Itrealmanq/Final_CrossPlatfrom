76f36fc0c2f9b899b11b2e0e890f0956c623ea66
Itrealmanq <161079958+Itrealmanq@users.noreply.github.com>
2025-03-28 01:35:14 +0700

    34

--- obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MiniprojectCross.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MiniprojectCross.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniprojectCross")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+32775493f0318e1cc83b1d50ddc9954704bb93e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+77a7427a4f26d67892a01ca91211d8e1e39d9d74")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniprojectCross")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniprojectCross")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
